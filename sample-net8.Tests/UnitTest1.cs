2a9213c137806ce0360154305dc8e94e31072d56
MayNittaya <nittaya.t@jaspal.co.th>
2025-09-29 20:03:36 +0700

    add unit tests for WeatherForecastController

---
+++ sample-net8.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace sample_net8.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ sample-net8.Tests/WeatherForecastControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Logging;
+using sample_net8.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample_net8.Tests
+{
+
+    public class WeatherForecastControllerTests
+    {
+        private readonly WeatherForecastController _controller;
+
+        public WeatherForecastControllerTests()
+        {
+            var logger = new LoggerFactory().CreateLogger<WeatherForecastController>();
+            _controller = new WeatherForecastController(logger);
+        }
+
+        [Fact]
+        public void Get_ShouldReturnFiveItems()
+        {
+            // Arrange & Act
+            var result = _controller.Get();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(5, result.Count());
+        }
+
+        [Fact]
+        public void Get_ShouldContainValidTemperatureRange()
+        {
+            // Arrange
+            var result = _controller.Get();
+
+            // Assert
+            Assert.All(result, item =>
+            {
+                Assert.InRange(item.TemperatureC, -20, 55);
+                Assert.False(string.IsNullOrEmpty(item.Summary));
+            });
+        }
+    }
+
+}
